c25e0e190b4946a0b6ef9107e34898170ede34e3
Pavel Potapov <paveln0366@gmail.com>
2024-09-08 17:47:18 +0300

    Added lesson 1

--- app/src/main/java/com/potapov/composeapp1/MainActivity.kt
+++ app/src/main/java/com/potapov/composeapp1/MainActivity.kt
@@ -3,45 +3,26 @@ package com.potapov.composeapp1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.potapov.composeapp1.ui.theme.ComposeApp1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ComposeApp1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            UserInfo(name = "John", age = 25)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    val a = 5
+    val name = "John"
+    Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeApp1Theme {
-        Greeting("Android")
-    }
+fun UserInfo(name: String, age: Int) {
+    Text(text = "Hello $name! You are $age years old")
 }
