da05957bf690ae86447ec1a4987d840c7adebb9c
Pavel Potapov <paveln0366@gmail.com>
2024-09-08 18:49:42 +0300

    Added lesson 2.4

--- app/src/main/java/com/potapov/composeapp1/MainActivity.kt
+++ app/src/main/java/com/potapov/composeapp1/MainActivity.kt
@@ -18,12 +18,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.potapov.composeapp1.ui.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimesTable()
+            InstagramProfileCard()
         }
     }
 }
